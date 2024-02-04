556df43a0400162fa92c7352a520f9e0825c77b5
mayconxrz@gmail.com <mayconxrz@gmail.com>
2024-02-04 19:40:02 -0400

    Create useSearch, custom hook para fazer pesquisas.

--- src/components/templates/Header/components/Search.tsx
+++ src/components/templates/Header/components/Search.tsx
@@ -1,19 +1,29 @@
+'use client';
+
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 
 import { Form } from '@/components/ui/Form';
 
+import { useSearch } from './hooks/useSearch';
+
 export const Search = () => {
+  const { register, handleSubmit } = useSearch();
+
   return (
-    <Form.Root className="hidden md:flex gap-x-1 items-center border-white border-[0.5px] rounded-full px-2">
+    <Form.Root
+      onSubmit={handleSubmit((data) => console.log(data))}
+      className="hidden md:flex gap-x-1 items-center border-white border-[0.5px] rounded-full px-2"
+    >
       <Form.Input
         type="text"
         placeholder="Pesquisar"
         id="search"
         className="w-48 h-9 border-none focus:bg-transparent"
         autoComplete="off"
+        {...register('search')}
       />
       <Form.Label htmlFor="search">
-        <MagnifyingGlassIcon className="w-[25px] h-[25px]" />
+        <MagnifyingGlassIcon type="submit" className="w-[25px] h-[25px]" />
       </Form.Label>
     </Form.Root>
   );
---
+++ src/components/templates/Header/components/hooks/useSearch.ts
@@ -0,0 +1,16 @@
+import { useForm } from 'react-hook-form';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+export const useSearch = () => {
+  const schema = z.object({
+    search: z.string().min(1)
+  });
+
+  const { register, handleSubmit } = useForm({
+    resolver: zodResolver(schema)
+  });
+
+  return { register, handleSubmit };
+};
