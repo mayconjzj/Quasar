04539c9dc7578b03611a47a2dfdf6b7d587c57a5
mayconxrz@gmail.com <mayconxrz@gmail.com>
2024-02-10 18:18:02 -0400

    New Commit

--- src/components/templates/Featured/Featured.tsx
+++ src/components/templates/Featured/Featured.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { Suspense } from 'react';
 
 import { loadFeatured } from '@/services/http';
@@ -15,68 +16,67 @@ export const Featured = async ({ mediaType }: MediaType) => {
 
   return (
     <Suspense fallback={<Loading />}>
-      <article
-        className="h-[85vh] bg-cover bg-center"
-        style={{
-          backgroundImage: `url(${process.env.NEXT_PUBLIC_TMDB_IMAGE_URL}/w1280${featured?.backdrop_path})`
-        }}
-      >
-        <div className="w-full h-full bg-gradient-to-b to-black/100 from-transparent">
-          <div className="w-full h-full bg-gradient-to-l to-black/100 from-transparent">
-            <div className="flex flex-col gap-3 p-2 md:px-[30px] h-full justify-end md:justify-center">
-              <div className="flex items-center justify-center"></div>
-              {featured?.title && (
-                <div className="font-bold text-5xl">{featured.title}</div>
-              )}
-              {featured?.name && (
-                <div className="font-bold text-5xl">{featured.name}</div>
-              )}
+      <article className="h-[85vh]">
+        <div className="relative w-full h-full">
+          <Image
+            src={`${process.env.NEXT_PUBLIC_TMDB_IMAGE_URL}/original${featured?.backdrop_path}`}
+            alt="Featured Image"
+            fill
+            objectFit="cover"
+            priority
+          />
+          <div className="absolute inset-0 bg-gradient-to-b to-black/100 from-transparent"></div>
+          <div className="absolute inset-0 bg-gradient-to-l to-black/100 from-transparent"></div>
+          <div className="absolute flex flex-col gap-3 p-2 md:px-[30px] h-full justify-end md:justify-center">
+            {featured?.title && (
+              <div className="font-bold text-5xl">{featured.title}</div>
+            )}
+            {featured?.name && (
+              <div className="font-bold text-5xl">{featured.name}</div>
+            )}
 
-              <div className="text-[18px] font-bold flex gap-3">
-                {featured?.vote_average && (
-                  <div>
-                    {featured.vote_average}{' '}
-                    <span className="text-[#46d369]">pontos</span>
-                  </div>
-                )}
-
-                {featured?.release_date && (
-                  <div>{firstDateYear(featured.release_date)}</div>
-                )}
-                {featured?.first_air_date && (
-                  <div>{firstDateYear(featured.first_air_date)}</div>
-                )}
-
-                {featured?.number_of_seasons && (
-                  <div>{featured.number_of_seasons} temporadas</div>
-                )}
-
-                {featured?.runtime && (
-                  <div>{featured.runtime + ' minutos'}</div>
-                )}
-              </div>
-              {featured?.overview && (
-                <div className="max-w-[600px] text-[#aaa]">
-                  {featured.overview.slice(0, 350)}
-                  {featured.overview.length > 350 && '...'}
-                </div>
-              )}
-
-              {featured && (
-                <div className="flex gap-2">
-                  <Button>Detalhes</Button>
-                </div>
-              )}
-
-              {featured?.genres && (
+            <div className="text-[18px] font-bold flex gap-3">
+              {featured?.vote_average && (
                 <div>
-                  <span className="font-bold">Gêneros: </span>
-                  <span className="text-[#aaa]">
-                    {featured.genres.map((genre) => genre.name).join(', ')}{' '}
-                  </span>
+                  {featured.vote_average}{' '}
+                  <span className="text-[#46d369]">pontos</span>
                 </div>
               )}
+
+              {featured?.release_date && (
+                <div>{firstDateYear(featured.release_date)}</div>
+              )}
+              {featured?.first_air_date && (
+                <div>{firstDateYear(featured.first_air_date)}</div>
+              )}
+
+              {featured?.number_of_seasons && (
+                <div>{featured.number_of_seasons} temporadas</div>
+              )}
+
+              {featured?.runtime && <div>{featured.runtime + ' minutos'}</div>}
             </div>
+            {featured?.overview && (
+              <div className="max-w-[600px] text-[#aaa]">
+                {featured.overview.slice(0, 350)}
+                {featured.overview.length > 350 && '...'}
+              </div>
+            )}
+
+            {featured && (
+              <div className="flex gap-2">
+                <Button>Detalhes</Button>
+              </div>
+            )}
+
+            {featured?.genres && (
+              <div>
+                <span className="font-bold">Gêneros: </span>
+                <span className="text-[#aaa]">
+                  {featured.genres.map((genre) => genre.name).join(', ')}{' '}
+                </span>
+              </div>
+            )}
           </div>
         </div>
       </article>
--- src/components/templates/Header/Header.tsx
+++ src/components/templates/Header/Header.tsx
@@ -4,7 +4,7 @@ import { Search } from './components/Search';
 
 export const Header = () => {
   return (
-    <header className="p-2 md:px-[30px] absolute flex justify-between w-screen items-center">
+    <header className="p-2 md:px-[30px] absolute flex justify-between w-screen items-center z-10">
       <Logo />
       <Search />
       <MainNav />
