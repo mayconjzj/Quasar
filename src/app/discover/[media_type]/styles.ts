a30152fdbc799450af16de14e71654f5c1d64a31
mayconxrz@gmail.com <mayconxrz@gmail.com>
2024-02-12 20:56:30 -0400

    New Commit

--- src/app/discover/[media_type]/styles.ts
+++ src/app/discover/[media_type]/styles.ts
@@ -1,3 +1,5 @@
 import tw from 'tailwind-styled-components';
 
 export const Container = tw.main``;
+export const TopRated = tw.article`h-[85vh] relative w-full text-primary-foreground`;
+export const Collection = tw.section`px-2 md:px-[30px]`;
