ef9fce62f9568869fe6cdf8e897663aed76d951b
mayconxrz@gmail.com <mayconxrz@gmail.com>
2024-02-14 12:34:12 -0400

    New Commit

---
+++ src/app/discover/[media_type]/[id]/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <div className="mt-[70px]">Loading</div>;
+}
--- src/app/search/styles.ts
+++ src/app/search/styles.ts
@@ -1,6 +1,6 @@
 import tw from 'tailwind-styled-components';
 
-export const Container = tw.main`pt-[70px] pb-8 md:px-8 px-2 flex flex-col gap-4 max-w-[1200px] m-auto`;
+export const Container = tw.main`mt-[70px] pb-8 md:px-8 px-2 flex flex-col gap-4 max-w-[1200px] m-auto`;
 export const Content = tw.section`w-full gap-4 flex flex-col`;
 
 export const Title = tw.h1`text-3xl font-black text-center`;
