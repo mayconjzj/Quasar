94ea8533ace235f08e3c665fcad0ec72f94ab33b
mayconxrz@gmail.com <mayconxrz@gmail.com>
2024-03-28 17:07:41 -0400

    New Commit

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,22 +1,22 @@
 import { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  return [
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
-      lastModified: new Date().toISOString()
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/discover/movies`,
-      lastModified: new Date().toISOString()
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/discover/series`,
-      lastModified: new Date().toISOString()
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/search`,
-      lastModified: new Date().toISOString()
-    }
-  ];
+  const sitemap: MetadataRoute.Sitemap = [];
+
+  sitemap.push({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+    lastModified: new Date().toISOString()
+  });
+
+  sitemap.push({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/discover/movies`,
+    lastModified: new Date().toISOString()
+  });
+
+  sitemap.push({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/discover/series`,
+    lastModified: new Date().toISOString()
+  });
+
+  return sitemap;
 }
